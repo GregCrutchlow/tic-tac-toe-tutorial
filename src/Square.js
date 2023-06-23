5bba476dea534839d29db9d4213e6b78fbf5d7c9
Greg Crutchlow <gregcrutchlow@gmail.com>
2023-06-23 17:40:45 -0400

    added project to repo

---
+++ src/Square.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useState } from "react";
+
+export default function Square() {
+    const [value, setValue] = useState(null);
+
+    function handleClick() {
+        setValue('X');
+    }
+    return <button className="square" onClick={handleClick}>{value}</button>
+}
